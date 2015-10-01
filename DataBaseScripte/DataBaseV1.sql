62d3b0bc73546a788507a213043a1304ec2ab3d6
tresor-ndeng <tresor.ndeng@gmail.com>
2015-09-30 20:09:40 -0400

    GestionnaireDeLivre

--- DataBaseScripte/DataBaseV1.sql
+++ DataBaseScripte/DataBaseV1.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `gestionnairebd` /*!40100 DEFAULT CHARACTER SET utf8 */;
+﻿CREATE DATABASE  IF NOT EXISTS `gestionnairebd` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `gestionnairebd`;
 -- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
 --
