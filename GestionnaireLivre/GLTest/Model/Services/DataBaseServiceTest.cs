485a43d9743ecf29d66a80541b23ed7add4b1af7
marcAndreTremblay <marct549@gmail.com>
2015-10-08 09:57:16 -0400

    forgot some stuff

--- GestionnaireLivre/GLTest/Model/Services/DataBaseServiceTest.cs
+++ GestionnaireLivre/GLTest/Model/Services/DataBaseServiceTest.cs
@@ -3,7 +3,10 @@ using NUnit.Framework.Constraints;
 using NUnit.Framework;
 using NUnit.Core;
 
+using System.Collections.Generic;
+
 using GestionnaireLivre.Model.Services;
+using GestionnaireLivre.Model.DataObject;
 
 namespace GLTest.Model.Services
 {
@@ -16,13 +19,80 @@ namespace GLTest.Model.Services
         [TestCase("testxx", "testxx", -1, TestName = "TestWrongUserCrendential")]
         public void TestCheckUserCrendential(string username, string password, int result)
         {
-            DataBaseService dbService = new DataBaseService();
+            DataBaseService sut = new DataBaseService();
 
-            int testResult = dbService.CheckLogInCredentials(username, password);
+            int sutResult = sut.CheckLogInCredentials(username, password);
 
-            Assert.AreEqual(result, testResult);
+            Assert.AreEqual(result, sutResult);
+        }
+        [Test]
+        public void TestGetBookConditionDataAcess()
+        {
+            DataBaseService sut = new DataBaseService();
+
+            List<BookCondition> conditionList = sut.BookCondition;
+
+            bool result = false;
+            if(conditionList.Count > 0) result= true;
+
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void TestGetTransactionStatusDataAcess()
+        {
+            DataBaseService sut = new DataBaseService();
+
+            List<TransactionStatus> resultList = sut.BookTransactionStatus;
+
+            bool result = false;
+            if (resultList.Count > 0) result = true;
+
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void TestGetUserTypeDataAcess()
+        {
+            DataBaseService sut = new DataBaseService();
+
+            List<UserType> resultList = sut.UserType;
+
+            bool result = false;
+            if (resultList.Count > 0) result = true;
+
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void TestGetTransactionTypeDataAcess()
+        {
+            DataBaseService sut = new DataBaseService();
+
+            List<TransactionType> resultList = sut.BookTransactionType;
+
+            bool result = false;
+            if (resultList.Count > 0) result = true;
+
+            Assert.AreEqual(true, result);
         }
 
 
+        [TestCase(-1, true,TestName = "User dont exist")]
+        [TestCase(100, false, TestName = "User dont exist")]
+        [TestCase(1,true, TestName = "User exist")]
+        public void TestRetriveSpecificUserInfo(int userId,bool expectedResult)
+        {
+            DataBaseService sut = new DataBaseService();
+
+            User userresult = sut.RetrieveSpecificUser(userId);
+            bool sutresult = false;
+            if(userresult.Id == userId)
+            {
+                sutresult = true;
+
+            }
+            Assert.AreEqual(expectedResult, sutresult);
+        }
     }
 }
